d375db9b558783170c8ac4d527e589fbd94ce05f
alexhrinyov <hrinyou29@gmail.com>
2023-04-20 16:59:48 +0300

    Создали 2 команды на двух кнопках

---
+++ Infrastructure/Commands/CloseAppCommand.cs
@@ -0,0 +1,25 @@
+﻿using RequestManager.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RequestManager.Infrastructure.Commands
+{
+    /// <summary>
+    /// Класс команды, демонстирующий способ, когда объект команды создается автоматически, без инициализации
+    /// в конструкторе
+    /// </summary>
+    internal class CloseAppCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+
+        public override void Execute(object? parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
--- ViewModels/RequestViewModel.cs
+++ ViewModels/RequestViewModel.cs
@@ -1,31 +1,58 @@
-﻿using RequestManager.ViewModels.Base;
+﻿using RequestManager.Infrastructure.Commands;
+using RequestManager.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace RequestManager.ViewModels
 {
     internal class RequestViewModel: ViewModel
     {
-        private int id;
-        private string date;
-        private string sender;
-        public int Id { get { return id; } }
-        public string Date 
-        {
-            get { return date; }
-            set => Set(ref date, value);
-        
-        }
-        public string Sender
-        {
-            get { return sender; }
-            set => Set(ref sender, value);
+        #region Примеры свойств
+        //private int id;
+        //private string date;
+        //private string sender;
+        //public int Id { get { return id; } }
+        //public string Date 
+        //{
+        //    get { return date; }
+        //    set => Set(ref date, value);
 
-        }
+        //}
+        //public string Sender
+        //{
+        //    get { return sender; }
+        //    set => Set(ref sender, value);
 
+        //}
+        #endregion
+
+        #region Команды
+        #region Пробная команда
+        public ICommand ShowMessageCommand { get; }
+        private void OnShowMessageCommandExecuted(object p)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+        private bool CanShowMessageCommandExecuted(object p) => true;
+
+        #endregion
+
+
+
+
+
+        #endregion
+
+        public RequestViewModel()
+        {
+            //свойство-объект команды инициализируется, передаются параметры методов(исполняющий метод и разрешающий)
+            ShowMessageCommand = new LambdaCommand(OnShowMessageCommandExecuted, CanShowMessageCommandExecuted);
+        }
     }
 }
