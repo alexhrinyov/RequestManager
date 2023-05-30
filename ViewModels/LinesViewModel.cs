bd02ecb5a3855baf05da99af5736ad4493a8a7b8
alexhrinyov <hrinyou29@gmail.com>
2023-05-30 17:27:45 +0300

    Поработал над окном линий

---
+++ ViewModels/LinesViewModel.cs
@@ -0,0 +1,25 @@
+﻿using RequestManager.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestManager.ViewModels
+{
+    internal class LinesViewModel:ViewModel
+    {
+
+
+
+
+
+
+
+
+        public LinesViewModel()
+        {
+
+        }
+    }
+}
