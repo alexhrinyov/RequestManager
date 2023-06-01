60a3c5ba705f4ee8e8f636811fe1a3cc0dd4bbe2
alexhrinyov <hrinyou29@gmail.com>
2023-06-01 16:59:17 +0300

    Добавил пару свойств и поработал над стилями

--- ViewModels/LinesViewModel.cs
+++ ViewModels/LinesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using RequestManager.ViewModels.Base;
+﻿using RequestManager.Data.Entities;
+using RequestManager.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,19 @@ namespace RequestManager.ViewModels
     {
 
 
+        #region Свойства
+        private List<Line> lines;
+        /// <summary>
+        /// Список линий в запросе
+        /// </summary>
+        public List<Line> Lines
+        {
+            get { return lines; }
+            set => Set(ref lines, value);
+        }
 
+
+        #endregion
 
 
 
