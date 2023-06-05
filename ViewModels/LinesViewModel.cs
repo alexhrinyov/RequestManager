512662597deeaef37561a811734ef2190d286847
alexhrinyov <hrinyou29@gmail.com>
2023-06-05 14:36:38 +0300

    стили, добавление линий

--- ViewModels/LinesViewModel.cs
+++ ViewModels/LinesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using RequestManager.Data.Entities;
 using RequestManager.Data.Repositories;
+using RequestManager.Infrastructure.Commands;
 using RequestManager.ViewModels.Base;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace RequestManager.ViewModels
 {
@@ -35,9 +37,52 @@ namespace RequestManager.ViewModels
             set => Set(ref requestId, value);
         }
 
+        
 
 
 
+        #endregion
+
+        #region Команды
+        public ICommand PushRequestDataCommand { get; }
+
+        private async void OnPushRequestDataCommand(object parameter)
+        {
+            foreach (var line in Lines)
+            {
+                if (line != null)
+                {
+                    line.RequestId = RequestId;
+                }    
+            }
+            try
+            {
+                if (requestRepository.SelectLinesById(RequestId).ToList() != Lines)
+                {
+                    await requestRepository.AddLinesAsync(Lines);
+                    MessageBox.Show("Отправлено в базу данных", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    foreach (var line in Lines)
+                    {
+                        if (line != null)
+                        {
+                           await  requestRepository.UpdateLineAsync(line);
+                            MessageBox.Show("Обновлено", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private bool CanPushRequestDataExecuted(object p) => true;
         #endregion
 
 
@@ -47,6 +92,8 @@ namespace RequestManager.ViewModels
         public LinesViewModel()
         {
             
+            requestRepository = new RequestRepository(new Data.RequestManagerContext());
+            PushRequestDataCommand = new LambdaCommand(OnPushRequestDataCommand, CanPushRequestDataExecuted);
         }
     }
 }
