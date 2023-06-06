0e27201a89cad1fc8863233f53998fa0eae09013
alexhrinyov <hrinyou29@gmail.com>
2023-06-06 15:10:58 +0300

    добавлена функция скрытия колонок, оформление

--- ViewModels/LinesViewModel.cs
+++ ViewModels/LinesViewModel.cs
@@ -37,7 +37,142 @@ namespace RequestManager.ViewModels
             set => Set(ref requestId, value);
         }
 
-        
+        #region Видимость колонок
+        private Visibility terminalHeadersSwgVisibility;
+
+        /// <summary>
+        /// Отмеченность чекбокса Тщ    
+        /// </summary>
+        private bool terminalHeadersSwgChecked = true;
+        /// <summary>
+        /// Отмеченность чекбокса Тщ
+        /// </summary>
+        public bool TerminalHeadersSwgChecked
+        {
+            get => terminalHeadersSwgChecked;
+            set
+            {
+                Set(ref terminalHeadersSwgChecked, value);
+
+            }
+        }
+
+        private Visibility terminalHeadersTRVisibility;
+
+        /// <summary>
+        /// Отмеченность чекбокса Ттр
+        /// </summary>
+        private bool terminalHeadersTRChecked = true;
+        /// <summary>
+        /// Отмеченность чекбокса Ттр
+        /// </summary>
+        public bool TerminalHeadersTRChecked
+        {
+            get => terminalHeadersTRChecked;
+            set
+            {
+                Set(ref terminalHeadersTRChecked, value);
+
+            }
+        }
+
+        private Visibility TVisibility;
+
+        /// <summary>
+        /// Отмеченность чекбокса Т
+        /// </summary>
+        private bool tChecked = true;
+        /// <summary>
+        /// Отмеченность чекбокса Т
+        /// </summary>
+        public bool TChecked
+        {
+            get => tChecked;
+            set
+            {
+                Set(ref tChecked, value);
+
+            }
+        }
+
+        private Visibility FBVisibility;
+
+        /// <summary>
+        /// Отмеченность чекбокса Пож. барьеров
+        /// </summary>
+        private bool fbChecked = true;
+        /// <summary>
+        /// Отмеченность Пож. барьеров
+        /// </summary>
+        public bool FbChecked
+        {
+            get => fbChecked;
+            set
+            {
+                Set(ref fbChecked, value);
+
+            }
+        }
+
+        private Visibility capsVisibility;
+
+        /// <summary>
+        /// Отмеченность чекбокса заглушек
+        /// </summary>
+        private bool capsChecked = true;
+        /// <summary>
+        /// Отмеченность заглушек
+        /// </summary>
+        public bool CapsChecked
+        {
+            get => capsChecked;
+            set
+            {
+                Set(ref capsChecked, value);
+
+            }
+        }
+
+        private Visibility expVisibility;
+
+        /// <summary>
+        /// Отмеченность чекбокса компенсаторов
+        /// </summary>
+        private bool expChecked = true;
+        /// <summary>
+        /// Отмеченность компенсаторов
+        /// </summary>
+        public bool ExpChecked
+        {
+            get => expChecked;
+            set
+            {
+                Set(ref expChecked, value);
+
+            }
+        }
+
+        private Visibility cabelBoxesVisibility;
+
+        /// <summary>
+        /// Отмеченность чекбокса кабельных коробок
+        /// </summary>
+        private bool cabelBoxesChecked = true;
+        /// <summary>
+        /// Отмеченность кабельных коробок
+        /// </summary>
+        public bool CabelBoxesChecked
+        {
+            get => cabelBoxesChecked;
+            set
+            {
+                Set(ref cabelBoxesChecked, value);
+
+            }
+        }
+
+
+        #endregion
 
 
 
